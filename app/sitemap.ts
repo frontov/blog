8e5edf450c05b6730d5d8140c09ef3a8544fe374
frontov <roman@frontov.ru>
2026-04-14 21:46:13 +0300

    Render sitemap from runtime data

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,8 @@ import type { MetadataRoute } from "next";
 import { readPosts } from "@/lib/posts";
 import { toAbsoluteUrl } from "@/lib/site";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await readPosts();
 
